a74615ed17032f0e6e15aa5930d79ce798186236
owickstrom <oskar@wickstrom.tech>
2020-08-10 11:13:00 +0200

    More exts

--- home.nix
+++ home.nix
@@ -174,11 +174,19 @@
 
   programs.vscode = {
     enable = true;
+    haskell = {
+      enable = true;
+      hie.enable = false;
+    };
     extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace
       (import ./vscode-extensions.nix).extensions;
     userSettings = {
       "update.channel" = "none";
       "[nix]"."editor.tabSize" = 2;
+      "workbench.colorTheme"= "Atom One Light";
+      "workbench.preferredLightColorTheme"= "Atom One Light";
+      "workbench.preferredDarkColorTheme"= "Atom One Dark";
+
     };
   };
 
--- vscode-extensions.nix
+++ vscode-extensions.nix
@@ -1,15 +1,69 @@
 { extensions = [
   {
+    name = "vscode-theme-onedark";
+    publisher = "akamud";
+    version = "2.2.2";
+    sha256 = "1d4nrl0d7zb9ddcvvrca6krj2ippchwfi2979fvzzswr70fnhc7a";
+  }
+  {
+    name = "vscode-theme-onelight";
+    publisher = "akamud";
+    version = "2.2.2";
+    sha256 = "1c70nnpm80hij6aq9qjlnzg93dbaz1hs2a9nxxzm3g3xbxahajdc";
+  }
+  {
+    name = "nix-env-selector";
+    publisher = "arrterian";
+    version = "0.1.2";
+    sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
+  }
+  {
     name = "Nix";
     publisher = "bbenoist";
     version = "1.0.1";
     sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
   }
   {
+    name = "nixfmt-vscode";
+    publisher = "brettm12345";
+    version = "0.0.1";
+    sha256 = "07w35c69vk1l6vipnq3qfack36qcszqxn8j3v332bl0w6m02aa7k";
+  }
+  {
+    name = "haskell";
+    publisher = "haskell";
+    version = "1.0.1";
+    sha256 = "0l82cp0jhgcwbczrms3a31wlgaxnxw6kslhv630agdr14xy6p1y1";
+  }
+  {
+    name = "language-haskell";
+    publisher = "justusadam";
+    version = "3.3.0";
+    sha256 = "1285bs89d7hqn8h8jyxww7712070zw2ccrgy6aswd39arscniffs";
+  }
+  {
     name = "vscode-sundial";
     publisher = "muuvmuuv";
     version = "2.3.0";
     sha256 = "0xsdnkzgzqsmdh7j5klvsd63my31lzmkllhw0dc2bzzv12nv4da2";
+  }
+  {
+    name = "vscode-purty";
+    publisher = "mvakula";
+    version = "0.4.1";
+    sha256 = "021r5cg6h229d2gfgd5a06iy0w5fw9563vxpfcs045nn559xpwxr";
+  }
+  {
+    name = "ide-purescript";
+    publisher = "nwolverson";
+    version = "0.21.3";
+    sha256 = "1bk8yia0fiaj7ppfdmgcikjpkjx7zw68fhp9q2pvb2jp8lbhgxfj";
+  }
+  {
+    name = "language-purescript";
+    publisher = "nwolverson";
+    version = "0.2.4";
+    sha256 = "16c6ik09wj87r0dg4l0swl2qlqy48jkavpp5i90l166x2mjw2b7w";
   }
   {
     name = "vim";
