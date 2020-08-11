9f3f1ae2ef859c44e7a46e649d9ce725538fa293
owickstrom <oskar@wickstrom.tech>
2020-08-11 10:10:28 +0200

    Sundial and kbd bindings in terminal

--- vscode.nix
+++ vscode.nix
@@ -8,12 +8,23 @@
     extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace
       (import ./vscode-extensions.nix).extensions;
     userSettings = {
+      # Basics
       "update.channel" = "none";
       "[nix]"."editor.tabSize" = 2;
-      "workbench.colorTheme" = "Atom One Dark";
+      # Sundial
+      "window.autoDetectColorScheme" = "false";
+      "sundial.latitude" = "";
+      "sundial.longitude" = "55.815";
+      "workbench.colorTheme" = "13.0227";
       "workbench.preferredLightColorTheme" = "Atom One Light";
       "workbench.preferredDarkColorTheme" = "Atom One Dark";
-
+      # Terminal
+      "terminal.integrated.allowChords" = "false";
+      "terminal.integrated.commandsToSkipShell" = [
+        "-workbench.action.terminal.focusFindWidget"
+        "-workbench.action.terminal.clear"
+        "-workbench.action.quickOpen"
+      ];
     };
   };
 }
