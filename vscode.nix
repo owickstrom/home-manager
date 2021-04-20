3c1e5d9f4491b3b488ba63e6431a40eb029b02b0
Oskar Wickström <oskar@wickstrom.tech>
2021-04-20 07:37:44 +0200

    Handle vscode settings with sync

--- vscode.nix
+++ vscode.nix
@@ -1,55 +1,5 @@
 { config, lib, pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    haskell = {
-      enable = true;
-      hie.enable = false;
-    };
-    extensions = (pkgs.vscode-utils.extensionsFromVscodeMarketplace
-      (import ./vscode-extensions.nix).extensions)
-      ++ [ (import ./vscode-specstrom.nix { inherit pkgs; }) ];
-    userSettings = {
-      # Basics
-      "update.channel" = "none";
-      "[nix]"."editor.tabSize" = 2;
-      "editor.fontFamily" = "Iosevka";
-      "editor.fontSize" = 13;
-      "editor.fontWeight" = 500;
-      # Theme
-      "window.autoDetectColorScheme" = "true";
-      "workbench.colorTheme" = "Atom One Dark";
-      "workbench.preferredLightColorTheme" = "Atom One Light";
-      "workbench.preferredDarkColorTheme" = "Atom One Dark";
-      # Terminal
-      "terminal.integrated.allowChords" = "false";
-      "terminal.integrated.commandsToSkipShell" = [
-        "-workbench.action.terminal.focusFindWidget"
-        "-workbench.action.terminal.clear"
-        "-workbench.action.quickOpen"
-      ];
-      "idris.idrisPath" = "${pkgs.idris2}/bin/idris2";
-      "idris.idris2Mode" = true;
-      "python.languageServer" = "Jedi";
-      "emmet.includeLanguages" = { "html-eex" = "html"; };
-      "window.menuBarVisibility" = "toggle";
-    };
-    keybindings = [
-      {
-        key = "alt+n";
-        command = "editor.action.marker.next";
-      }
-      {
-        key = "alt+shift+n";
-        command = "editor.action.marker.nextInFiles";
-      }
-      {
-        key = "alt+p";
-        command = "editor.action.marker.prev";
-      }
-      {
-        key = "alt+shift+p";
-        command = "editor.action.marker.prevInFiles";
-      }
-    ];
   };
 }
