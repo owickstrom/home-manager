76bf52c3241046fcfd856344f62d28a7e81495a2
Oskar Wickström <oskar@wickstrom.tech>
2023-11-10 11:12:18 +0100

    Tweaks

--- skhd.nix
+++ skhd.nix
@@ -33,6 +33,6 @@
 
     ctrl + alt + shift - f : yabai -m window --toggle float
 
-    alt + shift - return : open -n -a ~/Applications/Alacritty.app
+    alt + shift - return : open -n -a /Applications/Alacritty.app
   '';
 }
